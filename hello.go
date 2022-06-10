ad212ef310b680822a062063a3fe9fd32b613a1c
= <=>
2022-06-10 00:21:29 -0300

    Implementando um projeto com algumas funcoes

--- hello.go
+++ hello.go
@@ -2,17 +2,52 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
+const MONITORAR int = 1
+const INICIAR_LOGS int = 2
+const SAIR_DO_PROGRAMA int = 0
+
 func main() {
+
+	exibeInformacoes()
+	exibeMenu()
+	printaTipoDeComando(readComando())
+}
+
+func exibeInformacoes() {
 	nome := "Dan"
 	versao := 1.1
-	idade := 32
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("Versão do programa", versao)
+}
 
-	fmt.Println("Olá Sr.", nome, "Sua idade é:", idade)
-	fmt.Println("Este programa se encontra na versão: ", versao)
+func exibeMenu() {
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do sistema")
+}
 
-	fmt.Println("O tipo da variável nome é:", reflect.TypeOf(nome))
-	fmt.Println("O tipo da variável idade é:", reflect.TypeOf(idade))
+func readComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func printaTipoDeComando(comando int) {
+	switch comando {
+	case MONITORAR:
+		fmt.Println("Monitorando...")
+	case INICIAR_LOGS:
+		fmt.Println("Iniciando os logs...")
+	case SAIR_DO_PROGRAMA:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço esse comando!")
+		os.Exit(-1)
+	}
 }
---
+++ readme.md
@@ -0,0 +1,24 @@
+
+# Golang - Primeiros passos
+
+Estudando GOlang, desde o princípio, como declarar variável, pacotes, função, etc.
+
+### Módulo 1 - Introdução ao Golang.
+
+Todo método que vem de um pacote externo, inicia com letra maiúscula.
+
+![alt text](https://raw.githubusercontent.com/DanSaRecTech/primeiros-passos-golang/master/curso%20golang/fmt_Println.png)
+
+nota-se o uso do pacote fmt e seu método Println.
+
+Para buildar um arquivo em Go, é necessário ir até a pasta onde se encontra o projeto, via terminal:
+-- go build arquivo.go -> esse comando irá criar o arquivo.exe
+
+-- ./arquivo -> irá executar o arquivo. 
+Mas tem um caminho mais fácil para buildar e executar no mesmo comando
+-- go run arquivo.go
+
+### Módulo 2 - Variáveis
+
+Quando não atribui um valor à variável, do tipo numérico, ele assume 0. Para string, um campo vazio ('')
+A ling
